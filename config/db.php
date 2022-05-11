5950db84dbfdb830f9dd9dc6b61b350a90403407
Ivan Santos <201860430@ucc.mx>
2022-05-10 21:02:16 -0500

    Solo falta conexion db

--- config/db.php
+++ config/db.php
@@ -1,9 +1,10 @@
 <?php
-$db = mysqli_connect('localhost','root','','proyecto_ti');
+$db = mysqli_connect('administrandoando.me','root','proyectoti','proyecto_ti');
 
 if ($db === false) { //¿error?
     exit('Error en la conexión con la bd');
 }
+
 
 mysqli_set_charset($db, 'utf8');
 ?>
